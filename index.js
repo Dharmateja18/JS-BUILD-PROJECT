660f7fab395a746a357885e721d914ff27e9e4b2
Dharmateja18 <dharmatejamuddana@gmail.com>
2025-02-28 20:45:54 +0530

    Updated

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("Connected")
+// var state={
+//     tasklist: [
+//         {
+//             imageurl: "",
+//             tasktitle: "",
+//             tasktype: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageurl: "",
+//             tasktitle: "",
+//             tasktype: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageurl: "",
+//             tasktitle: "",
+//             tasktype: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageurl: "",
+//             tasktitle: "",
+//             tasktype: "",
+//             taskDescription: ""
+//         },
+//         {
+//             imageurl: "",
+//             tasktitle: "",
+//             tasktype: "",
+//             taskDescription: ""
+//         }
+//     ]
+// }
+
+const state={
+    tasklist: 
+}
+
+//DOM operations
+const taskContents= document.querySelector(".task_contents")
+const taskModal= document.querySelector(".task__modal__body")
+
+console.log(taskContents);
+console.log(taskModal)
+
